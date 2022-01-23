5face84c9284b2c8527874af668b6e9b837ec1c9
DashBookProject <69051383+DashBookProject@users.noreply.github.com>
2022-01-22 22:47:57 -0500

    Add files via upload

---
+++ Chapter-8/debug-demo.py
@@ -0,0 +1,32 @@
+# import ipdb
+from dash import Dash, dcc, html, Input , Output
+import plotly.express as px
+
+df = px.data.tips()
+print(df.head())
+
+app = Dash(__name__)
+
+app.layout = html.Div([
+    html.Div(id='graph-placeholder'),
+    dcc.Dropdown(id='size-selected',
+                 value='Female',
+                 options=[{'label': x, 'value': x} for x in df.sex.unique()])
+])
+
+@app.callback(
+    Output('graph-placeholder', 'children'),
+    Input('size-selected', 'value')
+)
+def update_graph(value):
+    # ipdb.set_trace()
+    dff = df[df.sex==value]
+    dff = dff[dff.day=='Mon']
+    fig = px.bar(dff, x = 'time', y = 'total_bill')
+    my_graph = dcc.Graph(figure=fig)
+
+    return my_graph
+
+
+if __name__ == '__main__':
+    app.run_server(debug=False, threaded=False, port=8000)
